a91bdc38b19d6e72d6a4310b840e4ffb13dd00aa
Matic Leva <vertical3life@gmail.com>
2015-11-17 17:23:35 +0100

    Added Car Controller

---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class CarController : MonoBehaviour {
+    private float enginePower = 50;
+    private float maxSteer = 2;
+    private float power;
+    private float steer;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        power = Input.GetAxis("Vertical") * enginePower * Time.deltaTime;
+        steer = Input.GetAxis("Horizontal") * maxSteer;
+
+        transform.Translate(transform.forward * power, Space.World);
+
+        if (Math.Abs(power) > 0 && Math.Abs(steer) > 0)
+        {
+            var rot = Vector3.zero;
+            rot.y += steer;
+            transform.Rotate(rot);
+        }
+
+        CarStats.speed = power;
+        CarStats.steer = steer;
+    }
+}
---
+++ Assets/Scripts/Stats.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Stats : MonoBehaviour {
+
+    Text text;
+
+    // Use this for initialization
+    void Start () {
+        text = GetComponent<Text>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        text.text = "Power: " + CarStats.speed + "\nSteer: " + CarStats.steer;
+	}
+}
+
+public static class CarStats
+{
+    public static float speed;
+    public static float steer;
+}
